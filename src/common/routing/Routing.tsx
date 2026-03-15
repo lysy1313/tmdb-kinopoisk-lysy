7d518ab97b5732e545fed3e8989d15cb38f80ba1
lysy <lysy.ilya1313@gmail.com>
2026-03-15 15:24:52 +0300

    feat: add categoryMovies page, pagination and styles

--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -1,10 +1,11 @@
 /* eslint-disable react-refresh/only-export-components */
 import { MainPage } from "@/features/MainPage/ui/MainPage";
+import { MoviesCategoryPage } from "@/features/MoviesCategoryPage/MoviesCategoryPage";
 import { Route, Routes } from "react-router";
 
 export const Path = {
   Main: "/",
-  CategoryMovies: "category_movies",
+  CategoryMovies: "/movies/:category",
   FilteredMovies: "filtered_movies",
   Search: "search",
   Favorites: "favorites",
@@ -13,6 +14,6 @@ export const Path = {
 export const Routing = () => (
   <Routes>
     <Route path={Path.Main} element={<MainPage />} />
-    {/* <Route path={Path.Login} element={<Login />} /> */}
+    <Route path={Path.CategoryMovies} element={<MoviesCategoryPage />} />
   </Routes>
 );

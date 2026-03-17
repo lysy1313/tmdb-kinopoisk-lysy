f21dea2f403cfad39aac37257edbadfe43dc3179
lysy <lysy.ilya1313@gmail.com>
2026-03-17 18:59:53 +0300

    feat: add movieDetail page, styles and other features

--- src/features/MainPage/api/movieApi.ts
+++ src/features/MainPage/api/movieApi.ts
@@ -1,12 +1,27 @@
-import { baseApi } from "@/app/api/baseApi";
-import type { MovieResponse } from "./movie.types";
+import { baseApi } from '@/app/api/baseApi';
+import type { MovieCredits, MovieDetails, MovieResponse } from './movie.types';
 
 export const movie = baseApi.injectEndpoints({
   endpoints: (build) => ({
-    getMovies: build.query<MovieResponse, { category: string; page?: number }>({
-      query: ({ category, page = 1 }) => `movie/${category}?page=${page}`,
+    getMovies: build.query<MovieResponse, { category?: string; page?: number }>({
+      query: ({ category = 'popular', page = 1 }) => `movie/${category}?page=${page}`,
+    }),
+    getOneMovie: build.query<MovieDetails, string>({
+      query: (movie_id) => `movie/${movie_id}`,
+    }),
+    getMovieSimilar: build.query<MovieResponse, string>({
+      query: (movie_id) => `movie/${movie_id}/similar`,
+    }),
+    getOneMovieCredits: build.query<MovieCredits, string>({
+      query: (movie_id) => `movie/${movie_id}/credits`,
     }),
   }),
 });
 
-export const { useGetMoviesQuery, useLazyGetMoviesQuery } = movie;
+export const {
+  useGetMoviesQuery,
+  useLazyGetMoviesQuery,
+  useGetOneMovieQuery,
+  useGetOneMovieCreditsQuery,
+  useGetMovieSimilarQuery,
+} = movie;

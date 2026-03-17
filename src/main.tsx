19017fab2572ff86ed80d1ebc980936656c4fe94
lysy <lysy.ilya1313@gmail.com>
2026-03-17 19:42:29 +0300

    feat: add theme-mode and fix other

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
-import { createRoot } from "react-dom/client";
-import App from "./app/ui/App.tsx";
+import { createRoot } from 'react-dom/client';
+import App from './app/ui/App.tsx';
 
-import "@/common/styles/global.scss";
-import { BrowserRouter } from "react-router";
-import { store } from "./app/model/store";
-import { Provider } from "react-redux";
+import '@/common/styles/global.scss';
+import { BrowserRouter } from 'react-router';
+import { Provider } from 'react-redux';
+import { store } from './app/store/store.ts';
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </BrowserRouter>
 );

f2debdda55aa96075d5d9c728f3ba69d5eaa69fa
lysy <lysy.ilya1313@gmail.com>
2026-03-26 00:43:09 +0300

    fix: header.tsx and styles

--- src/common/components/Header/NavLinks/NavLinks.tsx
+++ src/common/components/Header/NavLinks/NavLinks.tsx
@@ -1,24 +1,30 @@
 import { Path } from '@/common/routing/Routing';
-import { NavLink, useLocation } from 'react-router';
+import { NavLink, useLocation, type NavLinkRenderProps } from 'react-router';
 import styles from './NavLinks.module.scss';
 
 export const NavLinks = () => {
   const location = useLocation();
+
+  const getNavLinkClass = ({ isActive }: NavLinkRenderProps) =>
+    isActive ? `${styles.headerLink} ${styles.active}` : styles.headerLink;
+
+  const getLinkClass = (isActive: boolean) => (isActive ? `${styles.headerLink} ${styles.active}` : styles.headerLink);
+
   return (
     <>
-      <NavLink to={Path.Main} className={({ isActive }) => (isActive ? styles.active : '')}>
+      <NavLink to={Path.Main} className={getNavLinkClass}>
         Main
       </NavLink>
-      <NavLink to={'/movies/popular'} className={() => (location.pathname.startsWith('/movies/') ? styles.active : '')}>
+      <NavLink to={'/movies/popular'} className={getLinkClass(location.pathname.startsWith('/movies/'))}>
         Category Movies
       </NavLink>
-      <NavLink to={Path.FilteredMovies} className={({ isActive }) => (isActive ? styles.active : '')}>
+      <NavLink to={Path.FilteredMovies} className={getNavLinkClass}>
         Filtered Movies
       </NavLink>
-      <NavLink to={Path.Search} className={({ isActive }) => (isActive ? styles.active : '')}>
+      <NavLink to={Path.Search} className={getNavLinkClass}>
         Search
       </NavLink>
-      <NavLink to={Path.Favorites} className={({ isActive }) => (isActive ? styles.active : '')}>
+      <NavLink to={Path.Favorites} className={getNavLinkClass}>
         Favorites
       </NavLink>
     </>
